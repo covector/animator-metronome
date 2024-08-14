799ca171d381a5d54960efcaf89986bfc371507a
covector <kinchancheuk@gmail.com>
2024-08-14 15:10:50 +0800

    roboto font import failed because i was stupid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -57,7 +57,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link
-    href="https://fonts.googleapis.com/css2?family=Roboto:wght@0,700&display=swap"
+    href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap"
     rel="stylesheet"
   />
 </svelte:head>
